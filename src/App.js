22150b952b7586aebb2885580968a4d9db267a37
Anant <aradhyaanant1@gmail.com>
2023-07-19 19:18:36 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -2,35 +2,19 @@ import React,{useState} from 'react';
 import Expenses from "./components/Expenses/Expenses"
 import NewExpense from './components/NewExpenses/NewExpense';
 import "./App.css";
-let DUMMY_EXPENSE=[
-    {
-        id:'e1',
-        title:'School fee',
-        amount:'250',
-        date:new Date(2021,5,6)
-
-    },
-    {
-        id:'e2',
-        title:'Gym fee',
-        amount:'350',
-        date:new Date(2021,3,7)
-    },
-    {
-        id:'e3',
-        title:'Tution fee',
-        amount:'450',
-        date:new Date(2021,8,9)
-    },
-    {
-        id:'e4',
-        title:'Loan fee',
-        amount:'550',
-        date:new Date(2021,1,12)
-    }
-]
+let DUMMY_EXPENSE=[];
 const App=()=>{
-    const [expenses,setExpenses]=useState(DUMMY_EXPENSE)
+    const [expenses,setExpenses]=useState(DUMMY_EXPENSE);
+    // fetch('http://localhost/sample-api/api/read.php').then(
+    //     response=>{
+    //         return response.json();
+    //     }
+    // ).then(
+    //     data=>{
+    //     console.log(data);
+    //     setExpenses(data);
+    //     }
+    // );
      const addExpenseHandler=(expense)=>{
         const updatedExpense=[expense,...expenses];
        setExpenses(updatedExpense) ;
--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -8,7 +8,7 @@ const ExpenseItem=(props)=>{
  return(
   
         <Card className="expense-item">
-           <ExpenseDate date={new Date(props.date)} />
+           {/* <ExpenseDate date={new Date(props.date)} /> */}
         <div className="expense-item__description"><h2>{ props.title }</h2>
         <div className="expense-item__price">${ props.amount }</div>
         </div>
